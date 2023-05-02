37419667c346a6a6a7324d270033714b55fa48e4
Abdelrahman Nasser <abdo2652149@gmail.com>
2023-05-03 00:41:42 +0300

    Added background

--- SDProject/SDProject/Controllers/HomeController.cs
+++ SDProject/SDProject/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Diagnostics;
 
 namespace SDProject.Controllers
 {
-    //second commit from Abdelrahman
+    
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
