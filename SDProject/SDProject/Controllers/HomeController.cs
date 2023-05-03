4575feae4441e6674eeb6bcc7c8a69c93ee73078
Abdelrahman Nasser <abdo2652149@gmail.com>
2023-05-03 11:55:34 +0300

    Error report.cshtml and not found data.css

--- SDProject/SDProject/Controllers/HomeController.cs
+++ SDProject/SDProject/Controllers/HomeController.cs
@@ -4,7 +4,9 @@ using System.Diagnostics;
 
 namespace SDProject.Controllers
 {
-    
+    /// <summary>
+    /// 
+    /// </summary>
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
